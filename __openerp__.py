bf93f6d656c7ed65b83224a436c5b531b2da8fae
katyagpe <salas-rodriguez@hotmail.com>
2017-11-09 21:39:44 -0600

    first commit

---
+++ __openerp__.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+{
+    'name': "campo en sale order (ventas)",
+
+    'summary': """ """,
+
+    'description': """ """,
+
+    'author': "Katya Guadalupe Salas Rodríguez",
+    'website': "",
+
+   
+    'category': 'Uncategorized',
+    'version': '0.1',
+
+    'depends': ['sale','product'],
+
+    
+    'data': [
+        'codigo_view.xml',
+    ],
+}
